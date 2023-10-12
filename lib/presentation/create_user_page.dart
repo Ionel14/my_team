a93adc3d368af2460c34f2df2a2f8711adb069ed
Ionel14 <53469111+Ionel14@users.noreply.github.com>
2023-10-12 10:41:29 +0300

    styled signIn page

--- lib/presentation/create_user_page.dart
+++ lib/presentation/create_user_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../actions/index.dart';
 import '../models/index.dart';
@@ -45,73 +46,110 @@ class _CreateUserPage extends State<CreateUserPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: const Center(
-                  child: Text("MyTeam")
+      body: Container(
+        decoration: const BoxDecoration(color: Colors.green),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 56.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Center(
+                    child: Text(
+                      'My Team',
+                      style: GoogleFonts.lato(
+                        textStyle: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+                    )
+                ),
               ),
             ),
-          ),
 
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: PendingContainer(builder: (BuildContext context, Set<String> pending) {
-              return Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Column(
-                  children: <Widget>[
-                    const Text("Sign in"),
-                    TextField(
-                      onChanged: (String value) {
-                        _email = value;
-                      },
-                      decoration: const InputDecoration(hintText: 'Email'),
-                      keyboardType: TextInputType.emailAddress,
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: PendingContainer(builder: (BuildContext context, Set<String> pending) {
+                return Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: Colors.deepOrangeAccent,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 16.0),
+                          child: Text(
+                            'Sign in',
+                            style: GoogleFonts.lato(
+                                textStyle: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 16.0),
+                          child: TextField(
+                            onChanged: (String value) {
+                              _email = value;
+                            },
+                            decoration: InputDecoration(
+                              hintText: 'Email',
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(16.0),
+                                borderSide: const BorderSide(
+                                  color: Colors.white70,
+                                ),
+                              )
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                        ),
+                        TextField(
+                          onChanged: (String value) {
+                            _password = value;
+                          },
+                          obscureText: true,
+                          keyboardType: TextInputType.visiblePassword,
+                          decoration: InputDecoration(
+                              hintText: 'Password',
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(16.0),
+                                borderSide: const BorderSide(
+                                  color: Colors.white70,
+                                ),
+                              )
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 32,
+                        ),
+                        if (pending.contains(CreateUser.pendingKey))
+                          const Center(
+                            child: CircularProgressIndicator(),
+                          )
+                        else ...<Widget>[
+                          ElevatedButton(
+                            onPressed: _onNext,
+                            child: const Text('Create'),
+                          ),
+                          const SizedBox(
+                            height: 32,
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pushReplacementNamed(context, '/login');
+                            },
+                            child: const Text('Login'),
+                          ),
+                        ]
+                      ],
                     ),
-                    TextField(
-                      onChanged: (String value) {
-                        _password = value;
-                      },
-                      obscureText: true,
-                      keyboardType: TextInputType.visiblePassword,
-                      decoration: const InputDecoration(
-                          hintText: 'Password',
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 32,
-                    ),
-                    if (pending.contains(CreateUser.pendingKey))
-                      const Center(
-                        child: CircularProgressIndicator(),
-                      )
-                    else ...<Widget>[
-                      ElevatedButton(
-                        onPressed: _onNext,
-                        child: const Text('Create'),
-                      ),
-                      const SizedBox(
-                        height: 32,
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pushReplacementNamed(context, '/login');
-                        },
-                        child: const Text('Login'),
-                      ),
-                    ]
-                  ],
-                ),
-              );
-            }),
-          ),
-        ],
+                  ),
+                );
+              }),
+            ),
+          ],
+        ),
       ),
     );
   }
